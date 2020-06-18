eb33d59a496d178c6a4ad9f6a2ed9d1e96f7cf7b
FoksaK <stranskyvo@gmail.com>
2020-06-18 21:16:06 +0200

    Vrací pixel ze zdroje

---
+++ PhotoR/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using SkiaSharp;
+
+namespace PhotoR
+{
+    public class Helpers
+    {
+        public static void Load(ref SKBitmap Bitmap, string src)
+        {
+            FileStream streamBG = new FileStream(src, FileMode.Open);
+
+            Bitmap = SKBitmap.Decode(streamBG);
+        }
+    }
+}
--- PhotoR/Program.cs
+++ PhotoR/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using PhotoR;
+using SkiaSharp;
 
 namespace PhotoR
 {
-    class Program
+    class Program: Helpers
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string uri = Console.ReadLine();
+            SKBitmap Image = null;
+
+            Load(ref Image, uri);
+
+            Console.WriteLine(Image.GetPixel(0, 0));
         }
     }
 }
